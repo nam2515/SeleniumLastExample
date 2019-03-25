9473e488fb439350f60a6992849144e79680ee44
nam2515 <46920027+nam2515@users.noreply.github.com>
2019-03-25 09:44:59 +0530

    Add files via upload

---
+++ LastSelenium/src/main/java/com/capgemini/test/StepPersonal.java
@@ -0,0 +1,52 @@
+package com.capgemini.test;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class StepPersonal {
+
+@Given("^that I have gone to Employee_Registration_form\\.jsp page$")
+public void that_I_have_gone_to_Employee_Registration_form_jsp_page() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@When("^I entered employee Num \"(.*?)\"$")
+public void i_entered_employee_Num(String arg1) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@When("^I entered employee Name \"(.*?)\"$")
+public void i_entered_employee_Name(String arg1) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@When("^I selected the state \"(.*?)\"$")
+public void i_selected_the_state(String arg1) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@When("^I selected the City \"(.*?)\"$")
+public void i_selected_the_City(String arg1) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@When("^I click on the next link$")
+public void i_click_on_the_next_link() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Then("^It will redirect to the Project\\.html file and title is \"(.*?)\"$")
+public void it_will_redirect_to_the_Project_html_file_and_title_is(String arg1) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+}
---
+++ LastSelenium/src/main/java/com/capgemini/test/StepProject.java
@@ -0,0 +1,41 @@
+package com.capgemini.test;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class StepProject {
+
+@Given("^that I have gone to Project\\.html page$")
+public void that_I_have_gone_to_Project_html_page() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@When("^I entered the Project Name \"(.*?)\"$")
+public void i_entered_the_Project_Name(String arg1) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@When("^I selected the platform \"(.*?)\"$")
+public void i_selected_the_platform(String arg1) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@When("^I clicked on the registration button$")
+public void i_clicked_on_the_registration_button() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Then("^It will registered the Employee$")
+public void it_will_registered_the_Employee() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+
+}
